3bf93a03dd6ebe4fd96fbeb8289d1939e42b039b
exORYON <64312895+exORYON@users.noreply.github.com>
2021-01-28 18:54:05 +0200

    template for ratings was added, minor improvements

--- script.js
+++ script.js
@@ -52,9 +52,16 @@ const actors = [{
 
 let nickname = null;
 
+let musicEnabled = true;
+let soundsEnabled = true;
+let notificationsEnabled = false;
+
 const startScreenContainer = document.querySelector(".start-screen");
 const nicknameInputContainer = document.querySelector(".nickname-input");
 const mainMenuContainer = document.querySelector(".main-menu");
+const settingsContainer = document.querySelector(".settings");
+const ratingsContainer = document.querySelector(".ratings");
+
 const submitNicknameButton = document.querySelector("#submit-nickname");
 
 startScreenContainer.onclick = function() {
@@ -128,10 +135,64 @@ function startGame() {
 
 function openSettings() {
     console.log("SETTINGS");
+    settingsContainer.style.display = "flex";
     mainMenuContainer.style.display = "none";
 }
 
 function openRating() {
     console.log("RATING");
+    ratingsContainer.style.display = "flex";
     mainMenuContainer.style.display = "none";
+}
+
+let toggle = document.querySelectorAll(".toggle__button");
+let toggleClickHandler = function (el) {
+    switchToggle(el);
+}
+
+for (const each of toggle) {
+    each.addEventListener("click", toggleClickHandler);
+}
+
+function switchToggle(element) {
+    let toggleId = element.target.id;
+    let state = null;
+    
+    switch (toggleId) {
+        case "sounds-toggle":
+            musicEnabled = musicEnabled ? false : true;
+            state = musicEnabled;
+        break;
+        case "music-toggle":
+            soundsEnabled = soundsEnabled ? false : true;
+            state = soundsEnabled;
+        break;
+        case "notifications-toggle":
+            notificationsEnabled = notificationsEnabled ? false : true;
+            state = notificationsEnabled;
+        break;
+        
+        default: break;
+    }
+
+    if (state) {
+        element.target.innerHTML = "ON";
+        element.target.style.left = "47%";
+        element.target.style.marginLeft = "0px";
+        element.target.style.backgroundColor = "#d4080ee7";
+    } else {
+        element.target.innerHTML = "OFF"
+        element.target.style.left = "0%";
+        element.target.style.marginLeft = "-1px";
+        element.target.style.backgroundColor = "#841B37";
+    }
+}
+
+const getBackArrow = document.querySelectorAll(".get-back");
+for (const each of getBackArrow) {
+    each.addEventListener("click", function() {
+        mainMenuContainer.style.display="flex"; 
+        settingsContainer.style.display = "none";
+        ratingsContainer.style.display = "none";
+    });
 }
